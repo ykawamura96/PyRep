b84e2def468b79694b5bd3477d37b764e2fc603e
y-kawamura <ykawamura96>
2021-06-17 15:59:35 +0900

    update suctin_cup

--- pyrep/robots/end_effectors/suction_cup.py
+++ pyrep/robots/end_effectors/suction_cup.py
@@ -13,9 +13,10 @@ class SuctionCup(Shape):
         suffix = '' if count == 0 else '#%d' % (count - 1)
         super().__init__(name + suffix if base_name is None else base_name + suffix)
         self._proximity_sensor = ProximitySensor('%s_sensor%s' % (name, suffix))
-        self._attach_point = ForceSensor('%s_connection%s' % (name, suffix))
+        self._attach_point = ProximitySensor('%s_sensor%s' % (name, suffix))
         self._old_parents: List[Object] = []
         self._grasped_objects: List[Object] = []
+        self.joints = [0.0]
 
     def grasp(self, obj: Object) -> bool:
         """Attach the object to the suction cup if it is detected.
@@ -33,6 +34,9 @@ class SuctionCup(Shape):
             self._grasped_objects.append(obj)
             self._old_parents.append(obj.get_parent())  # type: ignore
             obj.set_parent(self._attach_point, keep_in_place=True)
+            obj.set_dynamic(False)
+            obj.set_respondable(False)
+            self.joints = [1.0]
         return detected
 
     def release(self) -> None:
@@ -46,8 +50,11 @@ class SuctionCup(Shape):
             # Check if the object still exists
             if grasped_obj.still_exists():
                 grasped_obj.set_parent(old_parent, keep_in_place=True)
+                grasped_obj.set_dynamic(True)
+                grasped_obj.set_respondable(True)
         self._grasped_objects = []
         self._old_parents = []
+        self.joints = [0.0]
 
     def get_grasped_objects(self) -> List[Object]:
         """Gets the objects that are currently in the suction cup.
@@ -55,3 +62,40 @@ class SuctionCup(Shape):
         :return: A list of grasped objects.
         """
         return self._grasped_objects
+
+    def get_joint_positions(self) -> List[float]:
+        """If there are attached object(s), return [1.0], otherwise return [0.0]
+        """
+        return self.joints
+
+    def set_joint_positions(self, positions: List[float],
+                            disable_dynamics: bool = False) -> None:
+        """Currently always return None
+        """
+        return None
+
+    def set_joint_target_velocities(self, velocities: List[float]) -> None:
+        """Currently always return None
+        """
+        return None
+
+    def get_open_amount(self) -> List[float]:
+        """Gets the gripper open state. 1 means open, whilst 0 means closed.
+
+        :return: A list of floats between 0 and 1 representing the gripper open
+            state for each joint. 1 means open, whilst 0 means closed.
+        """
+        return self.joints
+
+    def actuate(self, amount: float, velocity: float) -> bool:
+        return True
+        # if amount > 0.5:  # open
+        #     self.release()
+        #     return True
+        # else:  # close
+        #     return True
+
+    def get_touch_sensor_forces(self) -> List[List[float]]:
+        """Currently alwasy 0
+        """
+        return [[0.0, 0.0, 0.0]]
